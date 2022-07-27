ddabd78ff6987814a701a675a20a40f23bb68b22
jinuklee <20181680@edu.hanbat.ac.kr>
2022-07-27 14:06:48 +0900

    시그니처 코드

--- src/function/src/index.ts
+++ src/function/src/index.ts
@@ -1,3 +1,6 @@
 import {printHello} from "./print.hello";
+import {printUser} from "./print.user";
 
 printHello();
+printUser('Migni'
+    , 26);
---
+++ src/function/src/print.user.ts
@@ -0,0 +1,4 @@
+export const printUser: (string, number) => void = function (name: string, age: number): void {
+    console.log(`User's name: ${name}`);
+    console.log(`User's age: ${age}`);
+};
