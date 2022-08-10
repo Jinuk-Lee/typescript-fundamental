fae0457afb1fab5f0fa82db5b153d3437bd99d40
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-11 03:53:49 +0900

    단원별 내용 정리

--- README.md
+++ README.md
@@ -6,11 +6,15 @@
 - [typescript-fundamental](https://jinuk.notion.site/TypeScript-0dcfa061d4904b409e36b6c63fd0987e)
   - 타입스크립트에 대해서 알아보고 기본적인 환경설정을 합니다.
   - 타입스크립트 프로젝트를 생성하고 모듈화에 대해서 알아봅니다.
-- [objects-types](https://jinuk.notion.site/objects-types-0a8b45adddb4411eb270513e518727ca)
+- [objects and types](https://jinuk.notion.site/objects-types-0a8b45adddb4411eb270513e518727ca)
   - 타입스크립트에서 제공하는 클래스의 기능과 키워드를 알아봅니다.
   - 정의된 인터페이스를 구현하는`implements`를 알아봅니다.
   - 추상 클래스를 정의하는 `abstract`에 대해 알아봅니다.
   - 클래스가 정적인 속성을 가지게 하는 `static`를 공부합니다.
   - 구조화된 객체의 속성들을 비구조화 하는 비구조화 할당에 대해 배워봅니다.
-- [function]()
-- 
+- [function and methods]()
+- [arrays, tuples and loop]()
+- [synchronous and blocking](https://jinuk.notion.site/synchronous-and-blocking-02d34d3860964e59a9e475d8a1028886)
+  - 동기와 비동기, 블로킹과 논블로킹에 대해서 알아봅니다.
+  - 콜백 지옥과 이를 쉽게 다루는 `Promise`클래스에 대해서 알아봅니다.
+  - 비동기를 간결하게 구현하게 도와주는 `async-await` 구문을 공부해봅니다.
--- src/ts-project3/src/index.ts
+++ src/ts-project3/src/index.ts
@@ -8,7 +8,7 @@ import {calculatorArea} from "./calculator-area";
 import Fish from "./fish";
 import {flyOrSwim} from "./fly-or-swim";
 import Bird from "./bird";
-import {readFile, readFileSync} from "fs";
+import {read, readFile, readFileSync} from "fs";
 import {readFilePromise} from "./read-file.promise";
 //
 //
@@ -78,15 +78,19 @@ import {readFilePromise} from "./read-file.promise";
 
 readFilePromise('./package.json')
     //패키지를 읽고 성공하면 출력하고 그다음 ts를 읽어서 성공했으면 출력
+    //중복 if문과 비슷하지만 아래에서 에러를 모두 잡아준다는것이 차이점
     .then((value: string) => {
-        console.log(value);
-        throw new Error('Hello error');
-        return readFilePromise('./tsconfig.json');
-    })
-    .then((value: string) => {
-        console.log('print tsconfig.json');
         console.log(value);
     })
     .catch((error) => {
         console.log(error);
     });
+const asyncFunction = async () => {
+    const result = await readFilePromise('./package.json');
+    console.log(result);
+    const result2 = await readFilePromise('./tsconfig.json');
+    console.log(result2);
+}
+
+asyncFunction()
+
